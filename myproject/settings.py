37b42e2c9ec47fd880f1ff243ad35dca3d083132
System Administrator <root@Urbans-MacBook-Air.local>
2013-07-25 19:04:02 +0200

    some more stuff

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,9 +21,7 @@ DATABASES = {
     }
 }
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default=os.environ["DATABASE_URL"])}
+
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -166,3 +164,7 @@ LOGGING = {
         },
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=os.environ["DATABASE_URL"])}
