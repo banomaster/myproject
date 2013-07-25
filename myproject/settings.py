ecf3c4d9511e130c9ca88078dfb4cd96182b2242
System Administrator <root@Urbans-MacBook-Air.local>
2013-07-25 18:24:07 +0200

    changed settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,6 +21,10 @@ DATABASES = {
     }
 }
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
