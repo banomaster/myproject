cc172c922c10aa88d75446cffdce527b93642fd9
System Administrator <root@Urbans-MacBook-Air.local>
2013-07-27 23:25:32 +0200

    Jaki tecn

--- myproject/settings.py
+++ myproject/settings.py
@@ -45,6 +45,10 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -60,6 +64,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -150,3 +155,5 @@ import dj_database_url
 DATABASES = {'default': dj_database_url.config(env='DATABASE_URL')}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
